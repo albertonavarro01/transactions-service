1d1ba89bf7b5201dcab591578be1641432f7859e
Alberto Navarro <albertonavarroiman1@gmail.com>
2025-12-18 19:20:48 -0500

    Mantener solo transactions-service

--- creditcards-mongodb/src/main/java/creditcards_mongodb/repository/CreditCardRepository.java
+++
@@ -1,7 +0,0 @@
-package creditcards_mongodb.repository;
-
-import creditcards_mongodb.model.CreditCard;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CreditCardRepository extends MongoRepository<CreditCard,String> {
-}
